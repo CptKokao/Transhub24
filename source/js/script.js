6859bbd4e7fc0e45ad59f3ce27d098728508c18b
CptKokao <pro-markov@yandex.ru>
2020-03-15 17:44:19 +0300

    финальные правки

--- source/js/script.js
+++ source/js/script.js
@@ -15,3 +15,24 @@ $('.sslider').slick({
   slidesToShow: 4,
   dots: true,
 });
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.querySelector(".header__nav");
+var logoBlack = document.querySelector(".logo-black");
+var logoColor = document.querySelector(".logo-color");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetHeight;
+
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
